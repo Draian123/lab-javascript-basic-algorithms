8d767d8e16d5fe7abd203c66d2b2d504a063a20d
Draian123 <beiteldennis3@yahoo.de>
2023-01-11 11:57:22 +0100

    first iteration

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Iteration 1: Names and Input
+const hacker1 = 'Dennis';
+console.log(`The drivers name is ${hacker1}.`)
 
+const hacker2 = 'Ben';
+console.log(`The navigator's name is ${hacker2}.`)
 
 // Iteration 2: Conditionals
 
