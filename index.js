7a5ec18a177b7e1418ee82f06b81f8ca1e37e37e
Draian123 <beiteldennis3@yahoo.de>
2023-01-11 15:16:43 +0100

    finished all bonus quests 4 real again

--- index.js
+++ index.js
@@ -68,3 +68,23 @@ function countOccurrences(str,word){
 
 
 console.log(countOccurrences(longText, "et"))
+
+
+//Bonus 2
+
+let phraseToCheck = "A man, a plan, a canal, Panama!";
+
+
+function palindrome(str) {
+ var re = /[^A-Za-z0-9]/g;
+ str = str.toLowerCase().replace(re, '');
+ var len = str.length;
+ for (var i = 0; i < len/2; i++) {
+   if (str[i] !== str[len - 1 - i]) {
+       return false;
+   }
+ }
+ return true;
+}
+
+console.log(palindrome(phraseToCheck))
